5c392887b392bfbf7096f054be7ee21c418b8678
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-02-10 21:30:22 -0600

    Add Footer-idea

---
+++ src/footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>
+                        About me
+                    </h1>
+                    <h2>
+                        I'm a full-stack web developer
+                        who loves to build things.
+                    </h2>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
