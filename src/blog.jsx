8d410b987c8d707589701444aaa7148e065ceb52
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-02-15 22:49:26 -0600

    Update blog.jsx

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Wraped from './components/wraped'
 
 function Blog() {
     return (
@@ -6,7 +7,23 @@ function Blog() {
             <section
                 className='bg-primary w-full h-screen text-white'
             >
-                
+                <Wraped>
+                    <div>
+                        <h1
+                            className='text-7xl font-bold tracking-wide pt-40'
+                        >
+                            Personal Blog
+                        </h1>
+                        <h2
+                            className='text-text-general text-2xl tracking-wide mt-8'
+                        >
+                            Web development thoughts and stories.
+                        </h2>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </Wraped>
             </section>
         </>
     )
