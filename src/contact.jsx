637813ba1a3063c379247f384c51ec0c6a52de05
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-02-22 22:23:27 -0600

    Update contact.jsx

--- src/contact.jsx
+++ src/contact.jsx
@@ -3,7 +3,31 @@ import React from 'react'
 function Contact() {
   return (
     <>
-        
+        <section
+            className='bg-primary w-full h-screen text-white'
+        >
+            <Wraped>
+                <div
+                    className='pb-20'
+                >
+                    <h1
+                        className='text-text-blue text-2xl tracking-wide pt-40 font-bold'
+                    >
+                        Work
+                    </h1>
+                    <h2
+                        className='text-text-white-gray text-7xl font-bold tracking-wide'
+                    >
+                        Contact
+                    </h2>
+                    <h3
+                        className='text-text-general text-2xl tracking-wide mt-8'
+                    >
+                        Get in touch with me anytime, through social media, e-mail, or phone number.
+                    </h3>
+                </div>
+            </Wraped>
+        </section>
     </>
   )
 }
