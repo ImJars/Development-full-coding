88ae797c138a80bf465ff38ff8f42bc1ddc44200
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-03-01 23:11:18 -0600

    Update projects.jsx

--- src/projects.jsx
+++ src/projects.jsx
@@ -42,6 +42,19 @@ function Projects() {
                             >
                                 Add accent colors for dynamic, flexible color use in your Tailwind CSS project.
                             </h3>
+                            <button
+                                className='mt-5 bg-text-purple text-white px-8 py-3 rounded-full bg-opacity-10'
+                            >
+                                <a 
+                                    href="#"
+                                    className='flex items-center text-sm text-text-purple font-semibold space-x-1'
+                                >
+                                    <h1>
+                                        learn more
+                                    </h1>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-3 w-3"><path d="M14.5 7.40192C16.5 8.55662 16.5 11.4434 14.5 12.5981L8.5 16.0622C6.5 17.2169 4 15.7735 4 13.4641L4 6.5359C4 4.2265 6.5 2.78312 8.5 3.93782L14.5 7.40192Z"></path></svg>
+                                </a>
+                            </button>
                         </div>
                     </div>
                 </Wraped>
