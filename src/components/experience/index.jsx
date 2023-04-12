cf92cb92d57f14d13d3db73a23db5d160b239283
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-04-11 18:30:17 -0600

    Update index-experience

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { animate, motion } from "framer-motion";
 
-function Index({ subtitle_1, subtitle_2, subtitle_3, subtitle_4 }) {
+function Index({ subtitle_1, subtitle_2, subtitle_3, subtitle_4, animate_1, animate_2, animate_3, animate_4 }) {
   return (
     <>
       <div className="-mt-48 hidden lg:block">
@@ -14,13 +15,12 @@ function Index({ subtitle_1, subtitle_2, subtitle_3, subtitle_4 }) {
                 Table of Contents
               </h1>
             </div>
-            <div className="p-3 py-4">
-              <ol className="flex flex-col gap-2 text-text-general text-sm">
-                <li className="px-5">{ subtitle_1 }</li>
-                <li className="px-5">{ subtitle_2 }</li>
-                <li className="px-5">{ subtitle_3 }</li>
-                <li className="px-5">{ subtitle_4 }</li>
-                
+            <div className="py-4">
+              <ol className="flex flex-col text-text-general text-sm">
+                <motion.li animate={animate_1} className="px-5 py-1">{ subtitle_1 }</motion.li>
+                <motion.li animate={animate_2} className="px-5 py-1">{ subtitle_2 }</motion.li>
+                <motion.li animate={animate_3} className="px-5 py-1">{ subtitle_3 }</motion.li>
+                <motion.li animate={animate_4} className="px-5 py-1">{ subtitle_4 }</motion.li>
               </ol>
             </div>
           </nav>
--- src/experience.jsx
+++ src/experience.jsx
@@ -5,10 +5,9 @@ import List from "./components/experience/list";
 import Wraped from "./components/wraped";
 import Footer from "./footer";
 import { useInView } from "react-intersection-observer";
-import { motion, useAnimation } from "framer-motion";
+import { animate, motion, useAnimation } from "framer-motion";
 
 function Experience() {
-
   const { ref, inView } = useInView({
     threshold: 0.1,
     triggerOnce: false,
@@ -24,54 +23,172 @@ function Experience() {
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.1
+          type: "spring",
+          duration: 0.4,
+          delay: 0.1,
         },
       });
       animateTitleExperience.start({
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.3
+          type: "spring",
+          duration: 0.4,
+          delay: 0.3,
         },
       });
       animateSubtitleExperience.start({
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.5
+          type: "spring",
+          duration: 0.4,
+          delay: 0.5,
         },
       });
     }
 
     if (!inView) {
       animateMiniTitleExperience.start({
-        x: '-100',
+        x: "-100",
         opacity: 0,
       });
       animateTitleExperience.start({
-        x: '-100',
+        x: "-100",
         pacity: 0,
       });
       animateSubtitleExperience.start({
-        x: '-100',
+        x: "-100",
         opacity: 0,
       });
     }
-  }, [inView, animateMiniTitleExperience, animateTitleExperience, animateSubtitleExperience])
-  
+  }, [
+    inView,
+    animateMiniTitleExperience,
+    animateTitleExperience,
+    animateSubtitleExperience,
+  ]);
+
+  const { ref: refAnimate, inView: inViewAnimate } = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+  const { ref: refAnimate2, inView: inViewAnimate2 } = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+  const { ref: refAnimate3, inView: inViewAnimate3 } = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+  const { ref: refAnimate4, inView: inViewAnimate4 } = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+
+  const animate1 = useAnimation();
+  const animate2 = useAnimation();
+  const animate3 = useAnimation();
+  const animate4 = useAnimation();
+
+  useEffect(() => {
+    if (inViewAnimate) {
+      animate1.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inViewAnimate2) {
+      animate2.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inViewAnimate3) {
+      animate3.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inViewAnimate4) {
+      animate4.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+
+    if (!inViewAnimate) {
+      animate1.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inViewAnimate2) {
+      animate2.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inViewAnimate3) {
+      animate3.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inViewAnimate4) {
+      animate4.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+  }, [
+    inViewAnimate,
+    animate1,
+    inViewAnimate2,
+    animate2,
+    inViewAnimate3,
+    animate3,
+    inViewAnimate4,
+    animate4,
+  ]);
 
   return (
     <>
-      <section ref={ ref } className="bg-primary w-full h-full text-white">
+      <section ref={ref} className="bg-primary w-full h-full text-white">
         <Wraped>
           <div className="pb-20">
-            <motion.h1 animate={ animateMiniTitleExperience } className="opacity-0 transform -translate-x-10 text-text-blue text-2xl tracking-wide pt-40 font-bold">
+            <motion.h1
+              animate={animateMiniTitleExperience}
+              className="opacity-0 transform -translate-x-10 text-text-blue text-2xl tracking-wide pt-40 font-bold"
+            >
               Work
             </motion.h1>
-            <motion.h2 animate={animateTitleExperience} className="opacity-0 transform -translate-x-10 text-text-white-gray text-7xl font-bold tracking-wide">
+            <motion.h2
+              animate={animateTitleExperience}
+              className="opacity-0 transform -translate-x-10 text-text-white-gray text-7xl font-bold tracking-wide"
+            >
               Experience
             </motion.h2>
-            <motion.h3 animate={ animateSubtitleExperience } className="opacity-0 transform -translate-x-10 w-2/3 text-text-general text-2xl tracking-wide mt-8">
+            <motion.h3
+              animate={animateSubtitleExperience}
+              className="opacity-0 transform -translate-x-10 w-2/3 text-text-general text-2xl tracking-wide mt-8"
+            >
               All about my freelance, self-employed, and professional work
               experience.
             </motion.h3>
@@ -90,107 +207,107 @@ function Experience() {
                   </h1>
                 </div>
                 <div>
-                  <Content
-                    title={"Self-employed"}
-                    company={"Hemat.id"}
-                    date={"(Jun 2022 - Jul 2022)"}
-                    description={
-                      <ol>
-                        <List
-                          description={`Improving the UI & UX of Femicam's desktop application based on React Electron and Material UI.`}
-                        />
-                        <List
-                          description={`Designing and creating React components for the application dashboard.`}
-                        />
-                        <List
-                          description={`Creating an image editor using Sharp.js.`}
-                        />
-                      </ol>
-                    }
-                  />
-                  <Content
-                    display={true}
-                    title={"Freelance"}
-                    company={"1928.id"}
-                    date={"(Jun 2022 - Jul 2022)"}
-                    description={
-                      <ol>
-                        <List
-                          description={`Created main site navigation (menu, footer, mobile drawer) using Tailwind CSS.`}
-                        />
-                        <List
-                          description={`Created product tiles, promotion cards, and other small components using Tailwind CSS.`}
-                        />
-                      </ol>
-                    }
-                    company_2={"Hemat.id"}
-                    date_2={"(Jun 2022 - Jul 2022)"}
-                    description_2={
-                      <ol>
-                        <List
-                          description={`Improving the UI & UX of Femicam's desktop application based on React Electron and Material UI.`}
-                        />
-                        <List
-                          description={`Designing and creating React components for the application dashboard.`}
-                        />
-                      </ol>
-                    }
-                  />
-                  <Content
-                    title={"Professional Work"}
-                    company={"Ciptaloka"}
-                    date={"(Jul 2016 - Jun 2021)"}
-                    description={
-                      <>
-                        <div
-                          className="space-y-6"
-                        >
-                          <p>
-                            My first and (currently) only professional work is
-                            at Ciptaloka. At Ciptaloka, I do many types of work,
-                            such as developing desktop, mobile, and web
-                            applications.
-                          </p>
-                          <p>
-                            However, my work there is primarily focused on web
-                            development, and around the last 2 years I worked
-                            there, I have learned a lot about React,
-                            particularly when creating the v2 design editor.
-                          </p>
-                          <div
-                            className="space-y-4"
-                          >
-                            <h1>
-                              Here are the details of the work I did at
-                              Ciptaloka:
-                            </h1>
-                            <ol>
-                              <List
-                                description={`Created the v2 design editor layout using React.`}
-                              />
-                              <List
-                                description={`Redesigned the website's front page from vanilla CSS to use Foundation 6.`}
-                              />
-                              <List
-                                description={`Developed interactive UI on mobile editor using Framework7, Backbone.js, and Marionette.js.`}
-                              />
-                              <List
-                                description={`Created e-mail marketing design templates using Foundation for E-mails.`}
-                              />
-                              <List
-                                description={`Customized the blog.ciptaloka.com WordPress theme to match the looks and feels of the main site.`}
-                              />
-                            </ol>
+                  <div ref={refAnimate}>
+                    <Content
+                      title={"Self-employed"}
+                      company={"Hemat.id"}
+                      date={"(Jun 2022 - Jul 2022)"}
+                      description={
+                        <ol>
+                          <List
+                            description={`Improving the UI & UX of Femicam's desktop application based on React Electron and Material UI.`}
+                          />
+                          <List
+                            description={`Designing and creating React components for the application dashboard.`}
+                          />
+                          <List
+                            description={`Creating an image editor using Sharp.js.`}
+                          />
+                        </ol>
+                      }
+                    />
+                  </div>
+                  <div ref={refAnimate2}>
+                    <Content
+                      display={true}
+                      title={"Freelance"}
+                      company={"1928.id"}
+                      date={"(Jun 2022 - Jul 2022)"}
+                      description={
+                        <ol>
+                          <List
+                            description={`Created main site navigation (menu, footer, mobile drawer) using Tailwind CSS.`}
+                          />
+                          <List
+                            description={`Created product tiles, promotion cards, and other small components using Tailwind CSS.`}
+                          />
+                        </ol>
+                      }
+                      company_2={"Hemat.id"}
+                      date_2={"(Jun 2022 - Jul 2022)"}
+                      description_2={
+                        <ol>
+                          <List
+                            description={`Improving the UI & UX of Femicam's desktop application based on React Electron and Material UI.`}
+                          />
+                          <List
+                            description={`Designing and creating React components for the application dashboard.`}
+                          />
+                        </ol>
+                      }
+                    />
+                  </div>
+                  <div ref={refAnimate3}>
+                    <Content
+                      title={"Professional Work"}
+                      company={"Ciptaloka"}
+                      date={"(Jul 2016 - Jun 2021)"}
+                      description={
+                        <>
+                          <div className="space-y-6">
+                            <p>
+                              My first and (currently) only professional work is
+                              at Ciptaloka. At Ciptaloka, I do many types of
+                              work, such as developing desktop, mobile, and web
+                              applications.
+                            </p>
+                            <p>
+                              However, my work there is primarily focused on web
+                              development, and around the last 2 years I worked
+                              there, I have learned a lot about React,
+                              particularly when creating the v2 design editor.
+                            </p>
+                            <div className="space-y-4">
+                              <h1>
+                                Here are the details of the work I did at
+                                Ciptaloka:
+                              </h1>
+                              <ol>
+                                <List
+                                  description={`Created the v2 design editor layout using React.`}
+                                />
+                                <List
+                                  description={`Redesigned the website's front page from vanilla CSS to use Foundation 6.`}
+                                />
+                                <List
+                                  description={`Developed interactive UI on mobile editor using Framework7, Backbone.js, and Marionette.js.`}
+                                />
+                                <List
+                                  description={`Created e-mail marketing design templates using Foundation for E-mails.`}
+                                />
+                                <List
+                                  description={`Customized the blog.ciptaloka.com WordPress theme to match the looks and feels of the main site.`}
+                                />
+                              </ol>
+                            </div>
                           </div>
-                        </div>
-                      </>
-                    }
-                  />
-                  <Content 
-                    title={'What They Say'}
-                    company={'-'}
-                    date={'-'}
-                  />
+                        </>
+                      }
+                    />
+                  </div>
+                  <div ref={refAnimate4}>
+                    <Content title={"What They Say"} company={"-"} date={"-"} />
+                  </div>
                 </div>
               </div>
             </div>
@@ -199,6 +316,10 @@ function Experience() {
               subtitle_2={"Freelance"}
               subtitle_3={"Professional Work"}
               subtitle_4={"What They Say"}
+              animate_1={animate1}
+              animate_2={animate2}
+              animate_3={animate3}
+              animate_4={animate4}
             />
           </div>
           <div className="mt-20">
