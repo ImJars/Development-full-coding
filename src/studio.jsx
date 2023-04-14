547e12cd52cabae21cfabe0ca6efd9509aba4930
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-04-13 22:27:05 -0600

    Update studio-table-animate

--- src/studio.jsx
+++ src/studio.jsx
@@ -12,7 +12,6 @@ import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 function Studio() {
-
   const { ref, inView } = useInView({
     threshold: 0.1,
     triggerOnce: false,
@@ -27,21 +26,27 @@ function Studio() {
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.1
+          type: "spring",
+          duration: 0.4,
+          delay: 0.1,
         },
       });
       animateTitleStudio.start({
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.3
+          type: "spring",
+          duration: 0.4,
+          delay: 0.3,
         },
       });
       animateSubtitleStudio.start({
         opacity: 1,
         x: 0,
         transition: {
-          type: 'spring', duration: .4, delay: 0.5
+          type: "spring",
+          duration: 0.4,
+          delay: 0.5,
         },
       });
     }
@@ -60,9 +65,38 @@ function Studio() {
         opacity: 0,
       });
     }
-  }, [inView, animateMiniTitleStudio, animateTitleStudio, animateSubtitleStudio]);
-  
+  }, [
+    inView,
+    animateMiniTitleStudio,
+    animateTitleStudio,
+    animateSubtitleStudio,
+  ]);
+
   const { ref: ref1, inView: inView1 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref2, inView: inView2 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref3, inView: inView3 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref4, inView: inView4 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref5, inView: inView5 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref6, inView: inView6 } = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+  });
+  const { ref: ref7, inView: inView7 } = useInView({
     threshold: 0.1,
     triggerOnce: false,
   });
@@ -86,6 +120,66 @@ function Studio() {
         },
       });
     }
+    if (inView2) {
+      animate2.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inView3) {
+      animate3.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inView4) {
+      animate4.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inView5) {
+      animate5.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inView6) {
+      animate6.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
+    if (inView7) {
+      animate7.start({
+        backgroundColor: "#1D263A",
+        color: "#60A5FA",
+        transition: {
+          type: "spring",
+          duration: 0.1,
+        },
+      });
+    }
 
     if (!inView1) {
       animate1.start({
@@ -93,20 +187,80 @@ function Studio() {
         color: "#CBD5E1",
       });
     }
-  }, [ inView1, animate1 ]);
-  
+    if (!inView2) {
+      animate2.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inView3) {
+      animate3.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inView4) {
+      animate4.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inView5) {
+      animate5.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inView6) {
+      animate6.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+    if (!inView7) {
+      animate7.start({
+        backgroundColor: "#161E31",
+        color: "#CBD5E1",
+      });
+    }
+  }, [
+    inView1,
+    animate1,
+    inView2,
+    animate2,
+    inView3,
+    animate3,
+    inView4,
+    animate4,
+    inView5,
+    animate5,
+    inView6,
+    animate6,
+    inView7,
+    animate7,
+  ]);
+
   return (
     <>
-      <section ref={ ref } className="bg-primary w-full h-full text-white">
+      <section ref={ref} className="bg-primary w-full h-full text-white">
         <Wraped>
           <div className="pb-20">
-            <motion.h1 animate={ animateMiniTitleStudio } className="opacity-0 transform -translate-x-10 text-text-blue text-2xl tracking-wide pt-40 font-bold">
+            <motion.h1
+              animate={animateMiniTitleStudio}
+              className="opacity-0 transform -translate-x-10 text-text-blue text-2xl tracking-wide pt-40 font-bold"
+            >
               Work
             </motion.h1>
-            <motion.h2 animate={ animateTitleStudio } className="opacity-0 transform -translate-x-10 text-text-white-gray text-7xl font-bold tracking-wide">
+            <motion.h2
+              animate={animateTitleStudio}
+              className="opacity-0 transform -translate-x-10 text-text-white-gray text-7xl font-bold tracking-wide"
+            >
               Studio
             </motion.h2>
-            <motion.h3 animate={ animateSubtitleStudio } className="opacity-0 transform -translate-x-10 w-2/3 text-text-general text-2xl tracking-wide mt-8">
+            <motion.h3
+              animate={animateSubtitleStudio}
+              className="opacity-0 transform -translate-x-10 w-2/3 text-text-general text-2xl tracking-wide mt-8"
+            >
               A work-from-home workspace that allows me to efficiently complete
               most tasks.
             </motion.h3>
@@ -138,49 +292,51 @@ function Studio() {
                   </p>
                 </div>
                 <div className="mt-10">
-                  <h1 className="text-4xl font-bold text-text-white-gray tracking-wide mb-5">
-                    Display
-                  </h1>
-                  <p className="text-text-general">
-                    The dual monitor setup maximizes my web development
-                    workflow. I can code on one screen while previewing live on
-                    the other — no more Alt + Tab necessary for faster, more
-                    efficient work!
-                  </p>
-                  <p className="text-text-general mt-5">
-                    Need a website with extra wide breakpoint? I've got you
-                    covered with my ultrawide monitor.
-                  </p>
-                  <h1
-                    className="text-2xl font-bold text-text-white-gray tracking-wide mt-5"
-                  >
-                    Primary
-                  </h1>
-                  <TablePrimary />
-                  <h1
-                    className="text-2xl font-bold text-text-white-gray tracking-wide mt-5"
-                  >
-                    Secondary
-                  </h1>
-                  <TableSecondary />
-                  <h1
-                    className="text-2xl font-bold text-text-white-gray tracking-wide mt-5"
-                  >
-                    Peripherals
-                  </h1>
-                  <TablePeripherals />
-                  <h1
-                    className="text-2xl font-bold text-text-white-gray tracking-wide mt-5"
-                  >
-                    Conferencing Devices
-                  </h1>
-                  <TableConferencingDevices />
-                  <h1
-                    className="text-2xl font-bold text-text-white-gray tracking-wide mt-5"
-                  >
-                    Others
-                  </h1>
-                  <TableOthers />
+                  <div ref={ref2}>
+                    <h1 className="text-4xl font-bold text-text-white-gray tracking-wide mb-5">
+                      Display
+                    </h1>
+                    <p className="text-text-general">
+                      The dual monitor setup maximizes my web development
+                      workflow. I can code on one screen while previewing live
+                      on the other — no more Alt + Tab necessary for faster,
+                      more efficient work!
+                    </p>
+                    <p className="text-text-general mt-5">
+                      Need a website with extra wide breakpoint? I've got you
+                      covered with my ultrawide monitor.
+                    </p>
+                  </div>
+                  <div ref={ref3}>
+                    <h1 className="text-2xl font-bold text-text-white-gray tracking-wide mt-5">
+                      Primary
+                    </h1>
+                    <TablePrimary />
+                  </div>
+                  <div ref={ref4}>
+                    <h1 className="text-2xl font-bold text-text-white-gray tracking-wide mt-5">
+                      Secondary
+                    </h1>
+                    <TableSecondary />
+                  </div>
+                  <div ref={ref5}>
+                    <h1 className="text-2xl font-bold text-text-white-gray tracking-wide mt-5">
+                      Peripherals
+                    </h1>
+                    <TablePeripherals />
+                  </div>
+                  <div ref={ref6}>
+                    <h1 className="text-2xl font-bold text-text-white-gray tracking-wide mt-5">
+                      Conferencing Devices
+                    </h1>
+                    <TableConferencingDevices />
+                  </div>
+                  <div ref={ref7}>
+                    <h1 className="text-2xl font-bold text-text-white-gray tracking-wide mt-5">
+                      Others
+                    </h1>
+                    <TableOthers />
+                  </div>
                 </div>
               </div>
             </div>
@@ -192,7 +348,6 @@ function Studio() {
               subtitle_3={"Peripherals"}
               subtitle_4={"Conferencing Devices"}
               subtitle_5={"Others"}
-
               animate_1={animate1}
               animate_2={animate2}
               animate_3={animate3}
