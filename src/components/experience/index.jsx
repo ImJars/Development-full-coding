35dcdb09814c197b519bd32f0b1feb62baf35bc5
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-04-17 22:13:24 -0600

    Update index.jsx

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
-function Index({ subtitle_1, subtitle_2, subtitle_3, subtitle_4, animate_1, animate_2, animate_3, animate_4 }) {
+function Index({
+  subtitle_1,
+  subtitle_2,
+  subtitle_3,
+  subtitle_4,
+  animate_1,
+  animate_2,
+  animate_3,
+  animate_4,
+  animate_scroll,
+}) {
   return (
     <>
       <div className="-mt-48 hidden lg:block">
@@ -14,13 +24,32 @@ function Index({ subtitle_1, subtitle_2, subtitle_3, subtitle_4, animate_1, anim
               <h1 className="text-text-white-gray text-sm font-bold tracking-wide">
                 Table of Contents
               </h1>
+              <motion.div
+                className="opacity-0 transform translate-x-10"
+                animate={animate_scroll}
+              >
+                <a
+                  href="#skills"
+                  className="text-xs text-text-blue border border-text-blue border-opacity-20 px-2 py-1 rounded-full"
+                >
+                  scroll to top
+                </a>
+              </motion.div>
             </div>
             <div className="py-4">
               <ol className="flex flex-col text-text-general text-sm">
-                <motion.li animate={animate_1} className="px-5 py-1">{ subtitle_1 }</motion.li>
-                <motion.li animate={animate_2} className="px-5 py-1">{ subtitle_2 }</motion.li>
-                <motion.li animate={animate_3} className="px-5 py-1">{ subtitle_3 }</motion.li>
-                <motion.li animate={animate_4} className="px-5 py-1">{ subtitle_4 }</motion.li>
+                <motion.li animate={animate_1} className="px-5 py-1">
+                  {subtitle_1}
+                </motion.li>
+                <motion.li animate={animate_2} className="px-5 py-1">
+                  {subtitle_2}
+                </motion.li>
+                <motion.li animate={animate_3} className="px-5 py-1">
+                  {subtitle_3}
+                </motion.li>
+                <motion.li animate={animate_4} className="px-5 py-1">
+                  {subtitle_4}
+                </motion.li>
               </ol>
             </div>
           </nav>
