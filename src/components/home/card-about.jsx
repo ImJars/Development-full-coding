2d80160e44450cbff22dea2330a4f8b883e19169
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-05-03 22:31:34 -0600

    Update card-about.jsx

--- src/components/home/card-about.jsx
+++ src/components/home/card-about.jsx
@@ -5,7 +5,7 @@ function CardAbout({ extraClass, icon, description, text }) {
         <>
             <div
                 className='w-full border border-text-general border-opacity-10
-                            py-3 px-2 rounded-3xl'
+                            py-4 px-2 rounded-3xl'
             >
                 <div
                     className='flex flex-col justify-center items-center'
@@ -25,7 +25,7 @@ function CardAbout({ extraClass, icon, description, text }) {
                         </div>
                         <h1
                             className='text-sm text-white bg-card bg-opacity-50
-                                        pr-20 pl-11 py-2 rounded-full ml-4 font-semibold'
+                                        pr-32 pl-11 py-2 rounded-full ml-4 font-semibold'
                         >
                             { description }
                         </h1>
