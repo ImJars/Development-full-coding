3155bef6f6f6211d2597af71f682d72b4de8ace2
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-06-23 22:44:08 -0600

    Update text-home.jsx

--- src/components/home/text-home.jsx
+++ src/components/home/text-home.jsx
@@ -10,17 +10,17 @@ function TextHome({ text_one, text_two, text_three }) {
                     className='flex flex-col items-center space-y-2'
                 >
                     <span
-                        className='font-semibold text-6xl text-text-general'
+                        className='font-semibold text-2xl sm:text-6xl text-text-general'
                     >
                         { text_one }
                     </span>
                     <span
-                        className='font-semibold text-6xl text-text-general'
+                        className='font-semibold text-2xl sm:text-6xl text-text-general'
                     >
                         { text_two }
                     </span>
                     <span
-                        className='font-semibold text-6xl text-text-general'
+                        className='font-semibold text-2xl sm:text-6xl text-text-general'
                     >
                         { text_three }
                     </span>
