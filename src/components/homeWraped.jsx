46f0c66f664bc22fb777c97e767ef33e945c6a5c
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-07-10 23:26:02 -0600

    Update homeWraped.jsx

--- src/components/homeWraped.jsx
+++ src/components/homeWraped.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from "styled-components";
 
-function HomeWraped() {
+const HomeResponsiveWraped = styled.section`
+  @media (max-width: 639px) {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: .5rem;
+    padding-right: .5rem;
+  }
+  @media (min-width: 640px) {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
+`;
+
+function HomeWraped({ children }) {
     return (
         <>
-            
+            <HomeResponsiveWraped>
+                { children }
+            </HomeResponsiveWraped>
         </>
     )
 }
