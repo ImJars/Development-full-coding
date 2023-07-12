011733373d535502dbd23eb71de4ea6a637822fc
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-07-11 22:42:25 -0600

    Update card-home.jsx

--- src/components/home/card-home.jsx
+++ src/components/home/card-home.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function CardHome() {
   return (
     <>
-      <div className="border-divider-light relative z-10 flex-1 rounded-2xl border bg-white dark:border-divider-dark dark:bg-slate-900">
-        <div className="border-divider-light absolute inset-x-0 inset-y-8 z-[-1] border-t dark:border-divider-dark"></div>
-        <div className="border-divider-light absolute inset-y-0 inset-x-8 z-[-1] border-l dark:border-divider-dark"></div>
+      <div className="border-text-general relative z-10 flex-1 rounded-2xl border border-opacity-10 border-divider-dark bg-slate-900">
+        <div className="border-text-general border-opacity-10 absolute inset-x-0 inset-y-8 z-[-1] border-t border-divider-dark"></div>
+        <div className="border-text-general border-opacity-10 absolute inset-y-0 inset-x-8 z-[-1] border-l border-divider-dark"></div>
         <div className="-mt-0.5">
           <div className="mt-4 mr-2 ml-4 flex items-center gap-6 rounded-full bg-slate-100 dark:bg-slate-800">
             <div className="-m-2">
