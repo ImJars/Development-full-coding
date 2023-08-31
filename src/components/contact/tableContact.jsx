a90d6900df3be053bc26e4553668a728e8b151cc
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-08-30 21:23:59 -0600

    Update contact-tables

--- src/components/contact/tableContact.jsx
+++ src/components/contact/tableContact.jsx
@@ -8,30 +8,30 @@ function TableContact() {
         <table className="w-full">
           <thead className=" text-text-white-gray text-sm font-bold tracking-wide">
             <tr className="border-b border-text-general border-opacity-10">
-              <th scope="col" className="px-6 py-3 text-start">
+              <th scope="col" className="px-3 sm:px-6 py-3 text-start">
                 Name
               </th>
-              <th scope="col" className="px-6 py-3 text-start">
+              <th scope="col" className="px-0 sm:px-6 py-3 text-start">
                 Detail
               </th>
             </tr>
           </thead>
           <tbody className="text-text-general text-sm tracking-wide font-normal">
             <tr>
-              <td className="px-6 py-2">Address</td>
-              <td className="px-6 py-2 flex items-center">
+              <td className="px-3 sm:px-6 py-2">Address</td>
+              <td className="px-0 sm:px-6 py-2 flex items-center">
                 Hidalgo, Mexico
               </td>
             </tr>
             <tr>
-              <td className="px-6 py-2">E-mail</td>
-              <td className="px-6 py-2">
+              <td className="px-3 sm:px-6 py-2">E-mail</td>
+              <td className="px-0 sm:px-6 py-2">
                 <a 
                     href="#"
                     className="flex items-center text-text-blue"
                 >
                     <GrMail className="inline-block mr-2" />
-                    <span className="inline-block">
+                    <span className="inline-block text-xs sm:text-sm">
                         hi.legacyier@gmail.com
                     </span>
                 </a>
--- src/components/contact/tableSocialMedia.jsx
+++ src/components/contact/tableSocialMedia.jsx
@@ -4,25 +4,25 @@ import { HiExternalLink } from "react-icons/hi";
 function TableSocialMedia() {
   return (
     <>
-      <div className="relative overflow-x-auto rounded-lg border border-text-general border-opacity-10 my-10">
+      <div className="relative overflow-x-auto rounded-lg border border-text-general border-opacity-10 my-10 font-poppins">
         <table className="w-full">
-          <thead className=" text-text-white-gray text-sm font-bold tracking-wide">
+          <thead className=" text-text-white-gray text-sm font-bold">
             <tr className="border-b border-text-general border-opacity-10">
-              <th scope="col" className="px-6 py-3 text-start">
+              <th scope="col" className="px-3 sm:px-6 text-start">
                 Name
               </th>
-              <th scope="col" className="px-6 py-3 text-start">
+              <th scope="col" className="px-0 sm:px-6 py-3 text-start">
                 Detail
               </th>
             </tr>
           </thead>
-          <tbody className="text-text-general text-sm tracking-wide font-normal">
+          <tbody className="text-text-general text-sm font-normal">
             <tr>
-              <td className="px-6 py-2">LinkedIn</td>
-              <td className="px-6 py-2">
+              <td className="px-3 sm:px-6 py-2 text-xs sm:text-sm">LinkedIn</td>
+              <td className="px-0 sm:px-6 py-2">
                 <a 
                     href="https://www.linkedin.com/in/imjars" 
-                    className="flex items-center text-text-blue"
+                    className="flex items-center text-text-blue text-xs sm:text-sm"
                     target={"_blank"}
                     rel="noreferrer"
                 >
@@ -33,11 +33,11 @@ function TableSocialMedia() {
               </td>
             </tr>
             <tr>
-              <td className="px-6 py-2">Twitter</td>
-              <td className="px-6 py-2">
+              <td className="px-3 sm:px-6 py-2 text-xs sm:text-sm">Twitter</td>
+              <td className="px-0 sm:px-6 py-2">
                 <a 
                     href="https://twitter.com/Im_Jars" 
-                    className="flex items-center text-text-blue"
+                    className="flex items-center text-text-blue text-xs sm:text-sm"
                     target={"_blank"}
                     rel="noreferrer"
                 >
@@ -48,11 +48,11 @@ function TableSocialMedia() {
               </td>
             </tr>
             <tr>
-              <td className="px-6 py-2">Instagram</td>
-              <td className="px-6 py-2">
+              <td className="px-3 sm:px-6 py-2 text-xs sm:text-sm">Instagram</td>
+              <td className="px-0 sm:px-6 py-2">
                 <a 
                     href="https://www.instagram.com/jars.little" 
-                    className="flex items-center text-text-blue"
+                    className="flex items-center text-text-blue text-xs sm:text-sm"
                     target={"_blank"}
                     rel="noreferrer"
                 >
