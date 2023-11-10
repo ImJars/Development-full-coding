9f40138ee4b78c124589e4fc30bb4aaa7bb35c3e
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-11-09 22:57:20 -0600

    Update footer.jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,84 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 import { FiGithub, FiLinkedin, FiInstagram } from "react-icons/fi";
 import FooterLink from "./components/footer/footerLinks";
+
+const FooterSection = styled.section`
+  display: block;
+  .content-wrapped {
+    max-width: 1152px;
+  }
+`;
 
 function Footer() {
   return (
     <>
-      <div className="flex mx-auto flex-col font-poppins text-text-general">
-        <div className="flex justify-between">
-          <div className="w-1/4 mr-5">
-            <h1 className="text-text-general text-xs sm:text-sm font-semibold mb-4">About me</h1>
-            <h2 className="text-text-general mb-4 text-xs sm:text-sm">
-              I'm a full-stack web developer who loves to build things.
-            </h2>
-            <div className="flex space-x-4 text-xl">
-              <a
-                href="#"
-                className="hover:text-text-blue hover:transition hover:duration-500 flex"
-              >
-                𝕏
-              </a>
-              <a
-                href="#"
-                className="hover:text-text-blue hover:transition hover:duration-500 flex items-center"
-              >
-                <FiInstagram />
-              </a>
-              <a
-                href="#"
-                className="hover:text-text-blue hover:transition hover:duration-500 flex items-center"
-              >
-                <FiGithub />
-              </a>
-              <a
-                href="#"
-                className="hover:text-text-blue hover:transition hover:duration-500 flex items-center"
-              >
-                <FiLinkedin />
-              </a>
+      <footer className="bg-primary text-white font-poppins">
+        <FooterSection className="mx-1 sm:mx-0">
+          <div className="content-wrapped block mx-auto">
+            <div>
+              <div>
+                <h1 className="text-white hover:text-gray-300 bg-primary">
+                  hola
+                </h1>
+              </div>
+              <div></div>
             </div>
           </div>
-          <div className="w-3/4 mx-4 flex justify-between">
-            <div>
-              <h1 className="text-text-general text-xs sm:text-sm font-semibold mb-4">Work</h1>
-              <FooterLink link={"#"} text={"Contact"} />
-              <FooterLink link={"#"} text={"Experience"} />
-              <FooterLink
-                link={"#"}
-                classNames={"text-text-general"}
-                text={"Services"}
-              />
-              <FooterLink link={"#"} text={"Skills and Tools"} />
-              <FooterLink link={"#"} text={"Studio"} />
-            </div>
-            <div>
-              <h1 className="text-text-general text-xs sm:text-sm font-semibold mb-4">Learn</h1>
-              <FooterLink link={"#"} text={"Docs"} />
-              <FooterLink link={"#"} text={"Personal Blog"} />
-              <FooterLink link={"#"} text={"T.I.L"} />
-            </div>
-            <div>
-              <h1 className="text-text-general text-xs sm:text-sm font-semibold mb-4">
-                This Site
-              </h1>
-              <FooterLink link={"#"} text={"Design Concept"} />
-              <FooterLink link={"#"} text={"Source Code"} />
-              <FooterLink link={"#"} text={"Credits"} />
-            </div>
-          </div>
-        </div>
-        <div className="border-t border-text-general border-opacity-20 mt-8">
-          <div className="flex justify-between mt-5">
-            <h1 className="text-text-general text-xs">
-              © {new Date().getFullYear()}, Angel Ramirez
-            </h1>
-            <h2 className="text-text-general text-xs">
-              see the recent update on GitHub
-            </h2>
-          </div>
-        </div>
-      </div>
+        </FooterSection>
+      </footer>
     </>
   );
 }
