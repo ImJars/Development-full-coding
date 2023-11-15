568429be2a47e8eec2211bdc39f7e9b8dc8fa13e
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-11-14 23:01:07 -0600

    Update footer.jsx

--- src/footer.jsx
+++ src/footer.jsx
@@ -17,30 +17,41 @@ function Footer() {
       <footer className="bg-primary text-white font-poppins">
         <FooterSection className="mx-1 sm:mx-0">
           <div className="content-wrapped block mx-auto">
-            <div className="flex justify-end space-x-5 text-2xl">
-              <FooterIcons
-                classNames={""}
-                link={"https://github.com/ImJars"}
-                children={<FiGithub />}
-              />
-              <FooterIcons
-                classNames={""}
-                link={"https://www.linkedin.com/in/imjars/"}
-                children={<FiLinkedin />}
-              />
-              <FooterIcons
-                classNames={""}
-                link={"https://www.instagram.com/jars.little/"}
-                children={<FiInstagram />}
-              />
-              <FooterIcons
-                classNames={""}
-                link={"https://twitter.com/Im_Jars"}
-                children={"𝕏"}
-              />
-            </div>
             <div>
-
+              <div className="flex justify-end space-x-5 text-2xl">
+                <FooterIcons
+                  classNames={""}
+                  link={"https://github.com/ImJars"}
+                  children={<FiGithub />}
+                />
+                <FooterIcons
+                  classNames={""}
+                  link={"https://www.linkedin.com/in/imjars/"}
+                  children={<FiLinkedin />}
+                />
+                <FooterIcons
+                  classNames={""}
+                  link={"https://www.instagram.com/jars.little/"}
+                  children={<FiInstagram />}
+                />
+                <FooterIcons
+                  classNames={""}
+                  link={"https://twitter.com/Im_Jars"}
+                  children={"𝕏"}
+                />
+              </div>
+              <div className="flex">
+                <div className="w-1/3">
+                  <h1>
+                    About Me
+                  </h1>
+                </div>
+                <div className="w-2/3">
+                  <h1>
+                    Links
+                  </h1>
+                </div>
+              </div>
             </div>
           </div>
         </FooterSection>
