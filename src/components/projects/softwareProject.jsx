7dda23bcf6f0a53a14128b0f9c89e035bf7bef5e
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-01-18 22:59:03 -0600

    Update softwareProject.jsx

--- src/components/projects/softwareProject.jsx
+++ src/components/projects/softwareProject.jsx
@@ -7,9 +7,19 @@ function SoftwareProject() {
       <div className="border border-text-general border-opacity-10 rounded-xl">
         <div className="p-5">
           <div>
-            <div className="flex justify-center items-center space-x-4">
-              <FaFolderOpen className="text-4xl text-text-white-gray" />
-              <h1 className="text-text-white-gray font-bold text-lg">Jarsdev Website</h1>
+            <div className="flex flex-col items-center space-x-4">
+              <div className="w-full flex justify-start items-center">
+                <FaFolderOpen className="text-4xl text-text-white-gray" />
+                <h1 className="text-text-white-gray font-bold text-lg">
+                  Jarsdev Website
+                </h1>
+              </div>
+              <p className="p-0">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus animi perferendis beatae corporis nesciunt commodi
+                quibusdam natus esse odio a ratione sint error, aliquam iure
+                unde laudantium consequuntur veritatis quam?
+              </p>
             </div>
           </div>
         </div>
