9cf10d0c9497c9f02a18de73d05b24a0a2d917c1
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-01-23 22:03:00 -0600

    Update softwareProject.jsx

--- src/components/projects/softwareProject.jsx
+++ src/components/projects/softwareProject.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFolderOpen } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 
 function SoftwareProject() {
+  const [active, setActive] = useState(false);
+
   return (
     <>
       <div className="border bg-card hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300 ease-in-out border-text-general border-opacity-10 rounded-xl font-poppins">
@@ -28,6 +30,16 @@ function SoftwareProject() {
                 quibusdam natus esse odio a ratione sint error, aliquam iure
                 unde laudantium consequuntur veritatis quam?
               </p>
+              {
+                active ? (
+                  <div className="w-full flex justify-start text-sm mt-3">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Possimus animi perferendis beatae corporis nesciunt commodi
+                    quibusdam natus esse odio a ratione sint error, aliquam iure
+                    unde laudantium consequuntur veritatis quam?
+                  </div>
+                ) : null
+              }
               <div className="w-full flex justify-end mt-5">
                 <ul className="flex space-x-2 text-xs text-text-general">
                   <li>VS Code</li>
@@ -44,6 +56,11 @@ function SoftwareProject() {
                   </h1>
                   <li>Node</li>
                 </ul>
+              </div>
+              <div className="w-full flex justify-start">
+                <button onClick={ () => setActive(!active) } className="bg-text-blue px-6 py-1 rounded-lg bg-opacity-25 text-sm">
+                  { active ? "Cerrar" : "Ver más"}
+                </button>
               </div>
             </div>
           </div>
