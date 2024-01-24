393119cf0fed2c2995ee08b429a9220a6b8f1533
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-01-23 21:37:49 -0600

    Update softwareProject.jsx

--- src/components/projects/softwareProject.jsx
+++ src/components/projects/softwareProject.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { FaFolderOpen } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 function SoftwareProject() {
   return (
     <>
-      <div className="border bg-card border-text-general border-opacity-10 rounded-xl font-poppins">
+      <div className="border bg-card hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300 ease-in-out border-text-general border-opacity-10 rounded-xl font-poppins">
         <div className="p-5">
           <div>
             <div className="flex flex-col items-center space-x-4">
-              <div className="w-full flex justify-start items-center space-x-2">
-                <FaFolderOpen className="text-4xl text-text-white-gray" />
-                <h1 className="text-text-white-gray font-bold text-lg">
-                  Jarsdev Website
-                </h1>
+              <div className="w-full flex items-center justify-between">
+                <div className="flex items-center space-x-2">
+                  <FaFolderOpen className="text-3xl text-text-white-gray" />
+                  <h1 className="text-text-white-gray font-bold text-lg">
+                    Jarsdev Website
+                  </h1>
+                </div>
+                <div>
+                  <a href="#">
+                    <FiGithub className="text-xl text-text-white-gray" />
+                  </a>
+                </div>
               </div>
               <p className="w-full flex justify-start text-sm mt-3">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -22,21 +30,19 @@ function SoftwareProject() {
               </p>
               <div className="w-full flex justify-end mt-5">
                 <ul className="flex space-x-2 text-xs text-text-general">
-                  <li>
-                    VS Code
-                  </li>
-                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">&#8226;</h1>
-                  <li>
-                    React
-                  </li>
-                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">&#8226;</h1>
-                  <li>
-                    Tailwind
-                  </li>
-                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">&#8226;</h1>
-                  <li>
-                    Node
-                  </li>
+                  <li>VS Code</li>
+                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">
+                    &#8226;
+                  </h1>
+                  <li>React</li>
+                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">
+                    &#8226;
+                  </h1>
+                  <li>Tailwind</li>
+                  <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">
+                    &#8226;
+                  </h1>
+                  <li>Node</li>
                 </ul>
               </div>
             </div>
