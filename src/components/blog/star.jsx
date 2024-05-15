893d2ebd505801b3784613e7ede0ad734fab5694
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-05-14 23:41:07 -0600

    Update blog-like

--- src/components/blog/star.jsx
+++ src/components/blog/star.jsx
@@ -1,49 +1,62 @@
 import React, { useState, useEffect, useRef } from "react";
+import { FaStar, FaShare } from "react-icons/fa";
 
-function Star() {
-    const [favd, setFavd] = useState(false);
-    const favRef = useRef(null);
+function Star({ incrementar, decrementar, contador }) {
+  const [favd, setFavd] = useState(false);
+  const favRef = useRef(null);
 
-    useEffect(() => {
-        const favElem = favRef.current;
+  useEffect(() => {
+    const favElem = favRef.current;
 
-        const handleMouseDown = () => {
-            favElem.classList.add('favPress');
-        };
+    const handleMouseDown = () => {
+      favElem.classList.add("favPress");
+    };
 
-        const handleMouseUp = () => {
-            favElem.classList.remove('favPress');
-            setFavd(prevFavd => !prevFavd);
-        };
+    const handleMouseUp = () => {
+      favElem.classList.remove("favPress");
+      setFavd((prevFavd) => !prevFavd);
+    };
 
-        favElem.addEventListener('mousedown', handleMouseDown);
-        favElem.addEventListener('mouseup', handleMouseUp);
+    favElem.addEventListener("mousedown", handleMouseDown);
+    favElem.addEventListener("mouseup", handleMouseUp);
 
-        return () => {
-            favElem.removeEventListener('mousedown', handleMouseDown);
-            favElem.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, []);
+    return () => {
+      favElem.removeEventListener("mousedown", handleMouseDown);
+      favElem.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
 
-    const className = `fav ${favd ? 'favd' : ''}`;
+  const className = `fav ${favd ? "favd" : ""}`;
 
-    return (
-        <a ref={favRef} className={className} id="fav">
-            <svg className="stableHeart" viewBox="-1 0 18 15">
-                <defs>
-                    <path
-                        d="M16 4.872c0-4.9-6.894-5.8-8 .695C6.81-.928 0-.027 0 5.25c0 5.274 8 9.697 8 9.697s8-5.177 8-10.075z"
-                        id="heartPath"
-                    > </path>
-                </defs>
-
-                <use xlinkHref="#heartPath" />
-            </svg>
-            <svg className="floatHeart" viewBox="-1 0 18 15">
-                <use xlinkHref="#heartPath" />
-            </svg>
+  return (
+    <>
+      <div className="flex items-center space-x-1 flex-1 justify-center">
+        <a ref={favRef} className={className} onClick={incrementar} id="fav">
+          <svg className="stableHeart" viewBox="-1 0 18 15">
+            <defs>
+              <path
+                d="M16 4.872c0-4.9-6.894-5.8-8 .695C6.81-.928 0-.027 0 5.25c0 5.274 8 9.697 8 9.697s8-5.177 8-10.075z"
+                id="heartPath"
+              >
+                {" "}
+              </path>
+            </defs>
+            <use xlinkHref="#heartPath" />
+          </svg>
+          <svg className="floatHeart" viewBox="-1 0 18 15">
+            <use xlinkHref="#heartPath" />
+          </svg>
         </a>
-    );
+        <span className="text-sm no-seleccionable">{contador}</span>
+      </div>
+      <button
+        onClick={decrementar}
+        className="flex-1 bg-text-blue bg-opacity-10 flex justify-center py-2 rounded-lg"
+      >
+        <FaShare className="text-text-general" />
+      </button>
+    </>
+  );
 }
 
 export default Star;
--- src/components/blog/status.jsx
+++ src/components/blog/status.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { MdVerified } from "react-icons/md";
-import { FaStar, FaShare } from "react-icons/fa";
 
 import {  getFirestore, doc, getDoc, updateDoc, addDoc, collection, setDoc } from "firebase/firestore";
 import { db } from "../../Firebase/firebase";
@@ -43,33 +42,24 @@ function Status({ idContador, text, date }) {
         <div key={idContador} className="flex space-x-2 justify-between">
           <div className="flex items-center space-x-2">
             <div>
-              <img src="perfil.jpg" alt="" className="w-10 h-10 rounded-full" />
+              <img src="perfil.jpg" alt="" className="w-10 h-10 rounded-full no-seleccionable" />
             </div>
             <div>
               <div className="flex items-center space-x-1">
-                <h1 className="font-bold text-base">Angel Ramirez</h1>
+                <h1 className="font-bold text-base no-seleccionable">Angel Ramirez</h1>
                 <MdVerified className="text-sm text-text-blue" />
               </div>
-              <h2 className="text-sm text-text-general">@Im_Jars</h2>
+              <h2 className="text-sm text-text-general no-seleccionable">@Im_Jars</h2>
             </div>
           </div>
-          <div className="text-text-general">{date}</div>
+          <div className="text-text-general no-seleccionable">{date}</div>
         </div>
         <div className="mt-3">
           <p className="ml-12 font-sans">{text}</p>
         </div>
         <div className="mt-4">
           <div className="flex justify-between gap-3">
-            <button onClick={incrementar} className="flex-1 bg-text-blue bg-opacity-10 flex justify-center py-2 rounded-lg">
-              <div className="flex items-center space-x-1">
-              <FaStar className="text-text-general text-xl" />
-              <span className="text-sm">{contador}</span>
-              <Star />
-              </div>
-            </button>
-            <button onClick={decrementar} className="flex-1 bg-text-blue bg-opacity-10 flex justify-center py-2 rounded-lg">
-              <FaShare className="text-text-general" />
-            </button>
+            <Star incrementar={incrementar} decrementar={decrementar} contador={contador} />
           </div>
         </div>
       </div>
