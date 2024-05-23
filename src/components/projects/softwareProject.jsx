669bb27dc4b254ff6967ad3db2c45c21e4cd801a
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-05-23 00:17:49 -0600

    Update softwareProject.jsx

--- src/components/projects/softwareProject.jsx
+++ src/components/projects/softwareProject.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFolderOpen } from "react-icons/fa";
+import { FaFolderOpen, FaFolder } from "react-icons/fa";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 function SoftwareProject({
@@ -13,31 +13,34 @@ function SoftwareProject({
 }) {
   return (
     <>
-      <a href={projectLink}>
+      <a href={projectLink} className="group">
         <div
-          className='border bg-slate-900 hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300 ease-in-out border-text-general border-opacity-20 rounded-xl font-poppins'
+          className='border bg-slate-900 group-hover:bg-table-color group-hover:bg-opacity-80 group-hover:scale-105 group-hover:transition-all group-hover:duration-800 group-hover:ease-linear border-text-general border-opacity-20 rounded-xl font-sans'
         >
           <div className="p-5">
             <div>
               <div className="flex flex-col items-center space-x-4">
                 <div className="w-full flex items-center justify-between">
                   <div className="flex items-center space-x-2">
-                    <FaFolderOpen className="text-3xl text-text-white-gray" />
                     <div className="flex items-center">
-                      <h1 className="text-text-white-gray font-bold text-xl">
+                    <FaFolderOpen className="group-hover:opacity-100 opacity-0 ml-1.5 text-4xl text-text-white-gray group-hover:transition-all group-hover:duration-800 group-hover:ease-in-out absolute" />
+                    <FaFolder className="group-hover:opacity-0 ml-1.5 text-3xl text-text-white-gray group-hover:transition-all group-hover:duration-800 group-hover:ease-in-out absolute" />
+                    </div>
+                    <div className="flex items-center">
+                      <h1 className="ml-10 text-text-white-gray font-bold text-xl">
                         {title}
                       </h1>
                     </div>
                   </div>
                   <div>
-                    <FaExternalLinkAlt className="text-base text-text-white-gray" />
+                    <FaExternalLinkAlt className="text-base text-text-white-gray group-hover:text-text-purple group-hover:transition-all group-hover:duration-800 group-hover:ease-in-out" />
                   </div>
                 </div>
                 <p className="w-full flex justify-start text-sm mt-3 text-text-general">
                   {description}
                 </p>
                 <div className="w-full flex justify-end mt-5">
-                  <ul className="flex space-x-2 text-xs text-text-purple text-opacity-80">
+                  <ul className="flex space-x-2 text-xs text-text-purple">
                     <li>{skill_1}</li>
                     <h1 className="text-xs opacity-50 cursor-default text-text-white-gray">
                       &#8226;
